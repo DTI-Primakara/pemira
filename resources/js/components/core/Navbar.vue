81cf81746683b2b063018a8b9fa02d5cf558dba8
GungWira <wiradarma002@gmail.com>
2025-09-10 12:44:04 +0800

    Refactor navbar to component

---
+++ resources/js/components/core/Navbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <nav class="fixed top-0 left-0 flex w-full items-center justify-center bg-white px-10 py-4">
+        <div class="container flex w-full max-w-7xl items-center justify-between gap-4">
+            <Link href="/" class="flex items-start justify-start">
+                <img src="/images/logo-color.svg" alt="logo color pemira" class="h-12" />
+            </Link>
+            <div class="flex flex-row items-center justify-end gap-8">
+                <div class="flex flex-row items-center justify-end gap-5">
+                    <Link href="" class="font-dmsans text-base opacity-80 hover:opacity-100">Home</Link>
+                    <Link href="" class="font-dmsans text-base opacity-80 hover:opacity-100">About</Link>
+                    <Link href="" class="font-dmsans text-base opacity-80 hover:opacity-100">Candidates</Link>
+                    <Link href="" class="font-dmsans text-base opacity-80 hover:opacity-100">Timeline</Link>
+                    <Link href="" class="font-dmsans text-base opacity-80 hover:opacity-100">Contact</Link>
+                </div>
+                <a href="login">
+                    <button class="cursor-pointer bg-[#4342FF] px-5 py-3 font-unbounded text-sm text-white hover:bg-[#1c1cad]">VOTE NOW</button>
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
