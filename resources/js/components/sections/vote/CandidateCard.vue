08e0aa8feee5006ae987a00f1e6f69ff9e772c29
GungWira <wiradarma002@gmail.com>
2025-09-28 23:21:42 +0800

    Adjust ui on voting page

--- resources/js/components/sections/vote/CandidateCard.vue
+++ resources/js/components/sections/vote/CandidateCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref} from 'vue';
-import {InfoIcon, X} from 'lucide-vue-next'
+import { InfoIcon, X } from 'lucide-vue-next';
+import { ref } from 'vue';
 
 defineProps({
     number: {
@@ -29,7 +29,8 @@ defineProps({
     },
     mission: {
         type: String,
-        default: "Melaksanakan ekspedisi rasa tanpa batas dengan semangat 'Makan!', menjelajahi keanekaragaman rasa, mengutamakan porsi berdaulat, dan menciptakan kebersamaan di meja makan.",
+        default:
+            "Melaksanakan ekspedisi rasa tanpa batas dengan semangat 'Makan!', menjelajahi keanekaragaman rasa, mengutamakan porsi berdaulat, dan menciptakan kebersamaan di meja makan.",
     },
 });
 
@@ -41,36 +42,38 @@ const isOverlayVisible = ref<boolean>(false);
         class="group relative transform cursor-pointer overflow-hidden rounded-xl bg-blue-600 transition-all duration-300 hover:-translate-y-2"
         :class="isSelected ? 'ring-4 ring-blue-600 ring-offset-2' : 'ring-1 ring-gray-200 dark:ring-gray-700'"
     >
+        <!-- DECORATION -->
         <div class="absolute inset-y-0 left-0 w-20 opacity-30 md:opacity-90">
             <img src="/images/pattern-card.svg" alt="Pattern" class="h-full w-full object-cover" />
         </div>
         <div class="absolute inset-y-0 -right-5 w-20 opacity-30 md:opacity-90">
             <img src="/images/pattern-card-right.svg" alt="Pattern" class="h-full w-full object-cover" />
         </div>
+        <!-- DECORATION -->
 
         <button
             @click.stop="isOverlayVisible = !isOverlayVisible"
             v-show="!isOverlayVisible"
-            class="absolute top-4 right-4 z-30 flex items-center justify-center w-8 h-8 bg-black/30 rounded-full text-white md:hidden"
+            class="absolute top-4 right-4 z-30 flex h-8 w-8 items-center justify-center rounded-full bg-black/30 text-white md:hidden"
             aria-label="Tampilkan Visi & Misi"
         >
-            <InfoIcon/>
+            <InfoIcon />
         </button>
 
-        <div class="relative z-10 grid h-96 grid-cols-5 gap-4 px-8 pt-8">
-            <div class="col-span-2 flex flex-col gap-4">
+        <div class="relative z-10 grid h-96 grid-cols-9 gap-4 px-8 pt-8">
+            <div class="relative z-10 col-span-9 flex flex-col gap-4">
                 <div class="flex h-1/2 items-start justify-start rounded-lg">
-                    <span class="font-unbounded text-7xl font-extrabold text-white">{{ number }}</span>
+                    <span class="font-unbounded text-5xl font-extrabold text-white sm:text-6xl md:text-7xl">{{ number }}</span>
                 </div>
-                <div class="-ml-4 flex h-1/2 flex-col justify-center rounded-lg p-4">
-                    <h3 class="font-unbounded text-3xl font-bold text-white md:text-6xl">{{ name }}</h3>
-                    <p class="text-white opacity-80 md:text-lg">{{ position }}</p>
+                <div class="-ml-4 flex h-1/2 flex-col justify-center gap-2 rounded-lg p-4">
+                    <h3 class="font-unbounded text-2xl font-bold text-white sm:text-3xl md:text-4xl">{{ name }}</h3>
+                    <p class="text-sm text-white opacity-80 sm:text-base md:text-lg">{{ position }}</p>
                 </div>
             </div>
 
-            <div class="col-span-3 overflow-hidden rounded-lg">
+            <div class="absolute right-0 bottom-0 h-full overflow-hidden rounded-lg pt-6">
                 <img
-                    :src="imageUrl"
+                    :src="'/images/' + imageUrl"
                     :alt="name"
                     class="h-full w-full object-cover object-top grayscale transition-transform duration-500 group-hover:scale-105"
                 />
@@ -92,13 +95,20 @@ const isOverlayVisible = ref<boolean>(false);
                     <p class="opacity-80">{{ mission }}</p>
                 </div>
             </div>
-            <button
-                @click.stop="isOverlayVisible = false"
-                class="absolute top-4 right-4 z-30 text-white md:hidden"
-                aria-label="Tutup Visi & Misi"
-            >
-                <X/>
+            <button @click.stop="isOverlayVisible = false" class="absolute top-4 right-4 z-30 text-white md:hidden" aria-label="Tutup Visi & Misi">
+                <X />
             </button>
         </div>
     </div>
 </template>
+
+<style scoped>
+.group {
+    width: calc(calc(100% / 2) - 16px);
+}
+@media screen and (max-width: 864px) {
+    .group {
+        width: 100%;
+    }
+}
+</style>
--- resources/js/components/sections/vote/VotingStep.vue
+++ resources/js/components/sections/vote/VotingStep.vue
@@ -38,7 +38,7 @@ const selectCandidate = (candidateId: number) => {
 </script>
 
 <template>
-    <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
+    <div class="flex flex-row flex-wrap items-center justify-center gap-5 sm:gap-6 md:gap-8">
         <CandidateCard
             v-for="candidate in candidates"
             :key="candidate.id"
@@ -46,6 +46,8 @@ const selectCandidate = (candidateId: number) => {
             :name="candidate.name_candidates"
             :position="candidate.position"
             :image-url="candidate.image"
+            :vision="candidate.vision"
+            :mission="candidate.mission"
             :is-selected="selectedCandidate === candidate.id"
             @click="selectCandidate(candidate.id)"
         />
